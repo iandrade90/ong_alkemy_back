972216684cdc536ebbd5e5a8bf09d9140231d64e
leandro matias suarez <leandro_colocdk@hotmail.com>
2021-09-16 22:55:14 -0300

    Sendgrid

--- services/sendgrid.js
+++ services/sendgrid.js
@@ -1,50 +1,20 @@
 const sgMail = require('@sendgrid/mail');
 
-const RegisterSengrid = async (config) => {
+const EmailSendgrid = async (email) => {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-    //config es un objeto, el cual config.to recibe el email como body
-    config = {
-        to: config.email,
-        from: 'leandro_colocdk@hotmail.com',
-        subject: "Mail enviado desde la App Somos Más",
-        text: "Gracias por tu registro",
-        html: "<H2>Bienvenido/a a Somos Más</H2>",
-        mail_settings: {
-            sandbox_mode: {
-                enable: true // true (no se descuentan los email diarios)
-
-            }
-        }
-    }
-    // Email del cual estará configurado sendgrid
-    // config.from = "leandro_colocdk@hotmail.com"
-    return await sgMail.send(config);
-
-}
-
-const ContactSendgrid = async (config) => {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-    //config es un objeto, el cual config.to recibe el email como body
+    console.log(email)
 
     config = {
-        to: config.to,
+        to: email,
         from: 'leandro_colocdk@hotmail.com',
-        subject: "Mail enviado desde la App Somos Más",
-        text: "Gracias por contactarse con Somos Más",
-        html: "<H2>Gracias por contactarse con Somos Más</H2>,<p>Su correo llego correctamente!</p>",
-        // mail_settings: {
-        //     sandbox_mode: {
-        //         enable: true // true (no se descuentan los email diarios)
-        //     }
-        // }
+        subject: "Test",
+        text: "Test",
+        html: "Test",
     }
     // Email del cual estará configurado sendgrid
     // config.from = "leandro_colocdk@hotmail.com"
     return await sgMail.send(config);
 }
 module.exports = {
-    ContactSendgrid,
-    RegisterSengrid
+    EmailSendgrid
 };
