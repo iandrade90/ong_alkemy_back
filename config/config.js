c46b1c0b2b1f9483fb540c06cc91c1ec28803f92
leandro matias suarez <leandro_colocdk@hotmail.com>
2021-09-17 13:03:37 -0300

    dialect fix

--- config/config.js
+++ config/config.js
@@ -4,23 +4,23 @@ module.exports = {
     "development": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
-        "database":  process.env.DB_NAME,
+        "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
         "port": process.env.DB_PORT,
-        "dialect": "mysql"
+        "dialect": process.env.DIALECT
     },
     "test": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
-        "dialect": "mysql"
+        "dialect": process.env.DIALECT
     },
     "production": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
-        "dialect": "mysql"
+        "dialect": process.env.DIALECT
     }
 }
