724fef34660e71c9817a521ee1926a6fc08e9319
arian457 <ariancejas00@gmail.com>
2021-09-17 16:24:51 -0300

    removed contents of other ticket (fix)

--- controllers/organizationControllers.js
+++
@@ -1,7 +0,0 @@
-const { findOrganizationById } = require("../services/organizationsService")
-
-
-exports.findPublicOrganizations = (req, res, next) => {
-    const { id } = req.params
-   return findOrganizationById(id)
- }
--- services/organizationsService.js
+++
@@ -1,8 +0,0 @@
-
-const Repository = require("../repositories")
-const organizationRepository = new Repository
-const entity = "Organization"
-
-exports.findOrganizationById = id => {
-    return organizationRepository.findById(entity, id)
-}
