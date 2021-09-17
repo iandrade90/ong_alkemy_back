c2522adbf2f2a585f67c398925d2a9cc1558c18d
arian457 <ariancejas00@gmail.com>
2021-09-17 19:06:43 -0300

    improved controller 404 handler

--- controllers/organizationControllers.js
+++ controllers/organizationControllers.js
@@ -3,5 +3,10 @@ const { findOrganizationById } = require("../services/organizationsService")
 
 exports.findPublicOrganizations = (req, res, next) => {
     const { id } = req.params
-   return findOrganizationById(id)
+    findOrganizationById(id)
+        .then(response => {
+            !!response ? res.status(200).json(response) :
+                res.status(404).json("No hay organizacion con ese ID")
+        })
+    .catch(error => next(error))
  }
