ec014c3c3b9894fdd4489298d37d7271814d1fb3
arian457 <ariancejas00@gmail.com>
2021-09-20 14:09:47 -0300

    fixed name issue

--- controllers/organizationControllers.js
+++ controllers/organizationControllers.js
@@ -5,6 +5,6 @@ exports.findPublicOrganizations = async (req, res, next) => {
     const { id } = req.params
 
     const organizationFound = await findOrganizationById(id)
-    !!organizationFound? res.status(200).json(response) : res.status(404).json("No hay organizacion con ese ID")
+    !!organizationFound? res.status(200).json(organizationFound) : res.status(404).json("No hay organizacion con ese ID")
 
  }
