60ab0b3ffd33f4d19c1610fc426b90edbaabaf40
Octavio Peralta <octi.pro20@gmail.com>
2021-09-25 14:27:40 -0300

    added useful comments

--- middlewares/testimonials.js
+++ middlewares/testimonials.js
@@ -1,10 +1,13 @@
 const { check } = require("express-validator");
 const { customValidationResult } = require("./commons");
 
+
 const nameRequired = check("name", "Name field is required").notEmpty()
 const contentRequired = check("content", "Content field is required").notEmpty()
 
-const createValidations = [nameRequired, contentRequired, customValidationResult]
+//* Eventualmente se debe aplicar el middleware de token necesario 
+//* (REVISAR: dicho middleware no verifica que el token sea valido y ademas lanza un error no controlado cuando se envia nada como contenido)
+const createValidations = [  nameRequired, contentRequired, customValidationResult]
 
 module.exports = {
     createValidations
