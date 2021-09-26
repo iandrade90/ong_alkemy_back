a002db94be025feec775452121ac2879901489bd
acostamatias <matuu64@gmail.com>
2021-09-25 22:21:35 -0300

    Fix controller

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,10 +1,15 @@
 const contactsService = require('../services/contactsService');
 
 const contactList = async (req,res,next) => {
-    const list = await contactsService.findAll();
-    list.length < 1
-    ? res.status(200).json({message: 'No se encontraron contactos.'})
-    : res.status(200).json(list)
+    try {
+        const list = await contactsService.findAll();
+        list.length < 1
+        ? res.status(200).json({message: 'No se encontraron contactos.'})
+        : res.status(200).json(list)
+    } catch (error) {
+        next(error);
+    }
+    
 }
 
 module.exports = {
