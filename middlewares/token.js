c7c73107fa344ea4c56bfbcd0662155609a59a0e
arian457 <ariancejas00@gmail.com>
2021-09-27 15:04:55 -0300

    added update activities route, fixed tokenExists middleware error

--- middlewares/token.js
+++ middlewares/token.js
@@ -1,10 +1,9 @@
 const { decryptToken } = require("../services/tokenService")
 
 const tokenExists = (req, res, next) => {
-    
-    const data = decryptToken(req.header("Authorization")?.split(" ")[1])
-    if (data) {
-        req.data = data
+     const token = req.header("Authorization")?.split(" ")[1]
+    if (token) {
+        req.data = decryptToken(token)
         next()
     } else res.status(400).json("Missing token")
 }
