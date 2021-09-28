0a0e2b48507d9535322e4cff67db5a10b58c74f8
Gabriel Barbarin Gorostegui <gabi.barbarin@hotmail.com>
2021-09-28 22:20:00 +0000

    Crear migración y modelo de Miembros

---
+++ models/member.js
@@ -0,0 +1,18 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Member extends Model {
+    static associate(models) {}
+  }
+  Member.init(
+    {
+      name: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Member",
+    }
+  );
+  return Member;
+};
