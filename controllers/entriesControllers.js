7a58c645ac654d8925931328fad0fe1dd1bd6786
Gabriel Barbarin Gorostegui <gabi.barbarin@hotmail.com>
2021-09-28 16:51:32 -0300

    fix HTTP response status codes

--- controllers/entriesControllers.js
+++ controllers/entriesControllers.js
@@ -1,11 +1,13 @@
-const { customValidationResult: validationResult } = require('../middlewares/commons');
+const {
+  customValidationResult: validationResult,
+} = require("../middlewares/commons");
 const AllRepository = require("../repositories");
 const Repository = new AllRepository();
 const {
   findByIdAndEditEntry,
   findByIdEntry,
   createNews,
-  deleteById
+  deleteById,
 } = require("../services/entriesService");
 
 const entity = "Entries";
@@ -28,7 +30,7 @@ const putNews = async (req, res) => {
   try {
     const entry = await findByIdAndEditEntry(req.params.id, req.body);
     !entry
-      ? res.status(200).json({ message: "No existen entradas con ese ID" })
+      ? res.status(204).json({ message: "No existen entradas con ese ID" })
       : res.status(200).json(entry);
   } catch (error) {
     next(error);
@@ -61,7 +63,7 @@ const getEntry = async (req, res, next) => {
   try {
     const entry = await findByIdEntry(req.params.id);
     !entry
-      ? res.status(200).json({ message: "No existen entradas con ese ID" })
+      ? res.status(204).json({ message: "No existen entradas con ese ID" })
       : res.status(200).json(entry);
   } catch (error) {
     next(error);
@@ -73,22 +75,21 @@ const deleteNews = async (req, res, next) => {
   try {
     const entry = await findByIdEntry(id);
 
-    if(!entry){
-      res.status(200).json({message: 'No existen entradas con ese ID'})
+    if (!entry) {
+      res.status(200).json({ message: "No existen entradas con ese ID" });
     }
 
     deleteById(id);
-    res.status(200).json({message: 'Novedad eliminada'});
-
+    res.status(200).json({ message: "Novedad eliminada" });
   } catch (error) {
-    next(error); 
+    next(error);
   }
-}
+};
 
 module.exports = {
   postNews,
   getNews,
   putNews,
   getEntry,
-  deleteNews
+  deleteNews,
 };
