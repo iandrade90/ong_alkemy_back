92aa5c026ae9649854d243585d5c55e8984ba1ab
leandro matias suarez <leandro_colocdk@hotmail.com>
2021-09-30 13:59:35 -0300

    testimonials update

--- controllers/entriesControllers.js
+++ controllers/entriesControllers.js
@@ -30,7 +30,7 @@ const putNews = async (req, res) => {
   try {
     const entry = await findByIdAndEditEntry(req.params.id, req.body);
     !entry
-      ? res.status(204).json({ message: "No existen entradas con ese ID" })
+      ? res.status(200).json({ message: "No existen entradas con ese ID" })
       : res.status(200).json(entry);
   } catch (error) {
     next(error);
@@ -63,7 +63,7 @@ const getEntry = async (req, res, next) => {
   try {
     const entry = await findByIdEntry(req.params.id);
     !entry
-      ? res.status(204).json({ message: "No existen entradas con ese ID" })
+      ? res.status(200).json({ message: "No existen entradas con ese ID" })
       : res.status(200).json(entry);
   } catch (error) {
     next(error);
--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,5 +1,7 @@
+const Repositories = require("../repositories");
 const { uploadToBucket } = require("../services/S3");
 const testimonialService = require("../services/testimonialService");
+const Repository = new Repositories();
 
 const createTestimonial = async (req, res, next) => {
   const { name, content } = req.body;
@@ -9,11 +11,30 @@ const createTestimonial = async (req, res, next) => {
   try {
     const { Location: imageUrl } = await uploadToBucket(payload);
     const testimonialCreated = await testimonialService.create({ name, content, image: imageUrl });
-    res.status(201).json({ status: "ok", message: "Testimonio creado correctamente."  , data: testimonialCreated });
-    
+    res.status(201).json({ status: "ok", message: "Testimonio creado correctamente.", data: testimonialCreated });
+
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { createTestimonial };
+const updateTestimonial = async (req, res, next) => {
+  const { id } = req.params
+  const { name, content } = req.body;
+  const { image } = req.files;
+  const payload = { key: image.name, buffer: image.data };
+
+  try {
+    const { Location: imageUrl } = await uploadToBucket(payload);
+    const testimonialUpdated = await Repository.updatePayload("Testimonials", id, { name: name, content: content, image: imageUrl });
+    !testimonialUpdated
+      ? res.status(404).json({ message: "No se encuentra el testimonio con ese ID." })
+      : res.status(201).json({ message: "Testimonio actualizado.", testimonialUpdated });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+module.exports = { createTestimonial, updateTestimonial };
