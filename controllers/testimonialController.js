21693711e489615ad0a754abcea8bb23d441d242
leandro matias suarez <leandro_colocdk@hotmail.com>
2021-09-30 20:23:40 -0300

    testimonial delete

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,5 +1,7 @@
 const { uploadToBucket } = require("../services/S3");
 const testimonialService = require("../services/testimonialService");
+const AllRepository = require("../repositories");
+const Repository = new AllRepository();
 
 const createTestimonial = async (req, res, next) => {
   const { name, content } = req.body;
@@ -9,11 +11,26 @@ const createTestimonial = async (req, res, next) => {
   try {
     const { Location: imageUrl } = await uploadToBucket(payload);
     const testimonialCreated = await testimonialService.create({ name, content, image: imageUrl });
-    res.status(201).json({ status: "ok", message: "Testimonio creado correctamente."  , data: testimonialCreated });
-    
+    res.status(201).json({ status: "ok", message: "Testimonio creado correctamente.", data: testimonialCreated });
+
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { createTestimonial };
+const deleteTestimonial = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    console.log(id)
+
+    const testimonialDeleted = await Repository.deleteById("Testimonials", id);
+    !testimonialDeleted
+      ? res.status(200).json({ message: "No se pudo eliminar el testimonio con ese ID." })
+      : res.status(200).json({ message: "Testimonio eliminado!" });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createTestimonial, deleteTestimonial };
