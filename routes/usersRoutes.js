60ab14ff5ad75d53de898d22c08b673a132fd5f7
acostamatias <matuu64@gmail.com>
2021-10-02 16:21:26 -0300

    Endpoint users list

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,8 +1,13 @@
 var express = require('express');
 const { deleteUser } = require('../controllers/userControllers');
+const { getUsersList } = require('../services/userService');
 var router = express.Router();
+const { tokenExists } = require('../middlewares/token');
+const { isAdmin } = require('../middlewares/auth');
 
 /* GET users listing. */
 router.delete('/users/:id', deleteUser)
+
+router.get('/users', [tokenExists, isAdmin], getUsersList)
 
 module.exports = router;
