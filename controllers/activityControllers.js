b2d80c7ac0e74830fe4c158c3f995594cc9da7f0
Octavio Peralta <octi.pro20@gmail.com>
2021-10-06 21:29:08 -0300

    fully integrated activities section with front

--- controllers/activityControllers.js
+++ controllers/activityControllers.js
@@ -1,46 +1,71 @@
-
 const AllRepository = require("../repositories");
 const Repository = new AllRepository();
 const { uploadToBucket } = require("../services/S3");
 
 exports.createActivity = async (req, res, next) => {
-    try {
-        const { name, content } = req.body;
-        const { image } = req.files;
-        const payload = { key: image.name, buffer: image.data };
+  try {
+    const { name, content } = req.body;
+    // const { image } = req.files;
+    // const payload = { key: image.name, buffer: image.data };
+    // console.log(payload)
 
-        const { Location: imageUrl } = await uploadToBucket(payload);
+    // const { Location: imageUrl } = await uploadToBucket(payload);
 
-        const newActivity = await Repository.createPayload("Activities", { name: name, content: content, image: imageUrl });
-        !newActivity
-            ? res.status(200).json({ message: "No se pudo crear la actividad!" })
-            : res.status(201).json({ message: "Actividad creada con éxito!" });
-    } catch (error) {
-        next(error);
-    }
+    const newActivity = await Repository.createPayload("Activities", {
+      name: name,
+      content: content,
+      image:
+        "http://atrilco.com/wp-content/uploads/2017/11/ef3-placeholder-image.jpg",
+    });
+
+    !newActivity
+      ? res.status(200).json({ message: "No se pudo crear la actividad!" })
+      : res
+          .status(201)
+          .json({ message: "Actividad creada con éxito!", data: newActivity });
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.getActivity = async function (req, res, next) {
-    try {
-        const id = req.params.id
+  try {
+    const id = req.params.id;
 
-        const activity = await Repository.findByParams("Activities", { id })
-        if (activity) {
-            res.status(200).json(activity)
-        } else {
-            res.status(200).json({ message: "La actividad no existe" })
-        }
-    } catch (error) {
-        next(error);
+    const activity = await Repository.findByParams("Activities", { id });
+    if (activity) {
+      res.status(200).json(activity);
+    } else {
+      res.status(200).json({ message: "La actividad no existe" });
     }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.updateActivity = (req, res, next) => {
-    const { id } = req.params
-    const newParams = req.body
-    Repository.updatePayload("Activities", id, newParams)
-        .then(update => {
-            !update ? res.status(400).json("Activity not found") :
-                res.json(update)
-        })
-}
+  const { id } = req.params;
+  const newParams = req.body;
+  Repository.updatePayload("Activities", id, newParams).then(update => {
+    !update ? res.status(400).json("Activity not found") : res.json(update);
+  });
+};
+
+exports.getActivities = async (req, res, next) => {
+  try {
+    const activities = await Repository.findAll("Activities");
+    res.json({ status: "ok", data: activities });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteActivity = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Repository.deleteById("Activities", id);
+    res.json({ status: "ok", messaeg: "Actividad borrada correctamente" });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { getActivity, createActivity, updateActivity } = require("../controllers/activityControllers");
-
+const {
+  getActivity,
+  createActivity,
+  updateActivity,
+  getActivities,
+  deleteActivity,
+} = require("../controllers/activityControllers");
 
 const createActivityValidation = require("../middlewares/activityMiddleware");
 const { isAdmin } = require("../middlewares/auth");
 const { tokenExists } = require("../middlewares/token");
 
-
+router.get("/activities", tokenExists, isAdmin, getActivities);
 router.get("/activities/:id", tokenExists, isAdmin, getActivity);
-
 router.post("/activities", tokenExists, isAdmin, createActivityValidation, createActivity);
+router.put("/activities/:id", tokenExists, isAdmin, updateActivity);
+router.delete("/activities/:id", tokenExists, isAdmin, deleteActivity);
 
-router.put("/activities/:id", tokenExists, isAdmin, updateActivity)
 module.exports = router;
