86d365fa208d24b53f414cfe6e6fc31531e52208
acostamatias <matuu64@gmail.com>
2021-10-11 17:57:49 -0300

    delete contact route added

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { createContact, contactList } = require('../controllers/contactControllers');
+const { createContact, contactList, deleteContact } = require('../controllers/contactControllers');
 const createContactValidation = require("../middlewares/contact");
 const { isAdmin } = require('../middlewares/auth');
 const { tokenExists } = require('../middlewares/token');
@@ -7,5 +7,7 @@ const { tokenExists } = require('../middlewares/token');
 router.get('/contacts', [tokenExists, isAdmin], contactList);
 
 router.post("/contacts", tokenExists, createContactValidation, createContact);
+
+router.delete('/contacts/:id', [tokenExists, isAdmin], deleteContact);
 
 module.exports = router;
