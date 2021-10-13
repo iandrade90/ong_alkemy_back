b5ef125d4caf54e668681d54b8862b1ace2a65a1
Gabriel Barbarin Gorostegui <gabi.barbarin@hotmail.com>
2021-10-13 09:46:15 -0300

    fix contact routes

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -1,13 +1,17 @@
-const router = require('express').Router();
-const { createContact, contactList, deleteContact } = require('../controllers/contactControllers');
+const router = require("express").Router();
+const {
+  createContact,
+  contactList,
+  deleteContact,
+} = require("../controllers/contactControllers");
 const createContactValidation = require("../middlewares/contact");
-const { isAdmin } = require('../middlewares/auth');
-const { tokenExists } = require('../middlewares/token');
+const { isAdmin } = require("../middlewares/auth");
+const { tokenExists } = require("../middlewares/token");
 
-router.get('/contacts', [tokenExists, isAdmin], contactList);
+router.get("/contacts", [tokenExists, isAdmin], contactList);
 
-router.post("/contacts", tokenExists, createContactValidation, createContact);
+router.post("/contacts", createContactValidation, createContact);
 
-router.delete('/contacts/:id', [tokenExists, isAdmin], deleteContact);
+router.delete("/contacts/:id", [tokenExists, isAdmin], deleteContact);
 
 module.exports = router;
