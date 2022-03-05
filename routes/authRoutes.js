55a80ce7afdae2ca121da45f5c504632a0e23c7a
Ivan Andrade <ivandandrade90@gmail.com>
2022-03-05 14:00:44 -0300

    commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,20 @@
-const express = require("express")
-const { loginController, tokenController, registerController, updateUser } = require("../controllers/authControllers")
-const { loginValidations, registerValidations } = require("../middlewares/auth")
-const { tokenExists } = require("../middlewares/token")
-const router = express.Router()
+const express = require("express");
+const {
+  loginController,
+  tokenController,
+  registerController,
+  updateUser,
+} = require("../controllers/authControllers");
+const {
+  loginValidations,
+  registerValidations,
+} = require("../middlewares/auth");
+const { tokenExists } = require("../middlewares/token");
+const router = express.Router();
 
-
-
-router.post('/auth/login', loginValidations, loginController)
+router.post("/auth/login", loginValidations, loginController);
 router.post("/auth/register", registerValidations, registerController);
 router.put("/auth/:id", tokenExists, updateUser);
-router.get('/auth/me', tokenExists, tokenController)
+router.get("/auth/me", tokenExists, tokenController);
 
 module.exports = router;
